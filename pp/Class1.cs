7292c89b3e4551d3dcf05d28fec7373cbcb7cda6
олег <@DESKTOP-SSTKF2A>
2023-02-28 11:21:06 +0200

    Добавьте файлы проекта.

---
+++ pp/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace pp
+{
+    public class Param
+    {
+        public int h;
+        public Bitmap pic;
+    }
+
+    public class ParamWaterDrop : Param
+    {
+        public ParamWaterDrop()
+        {
+            h = 50;
+            pic = new Bitmap(@"D:\pipipupu\bb.png");
+        }
+    }
+    public class ParamTwoWaterDrop : Param
+    {
+        public ParamTwoWaterDrop()
+        {
+            h = 50;
+            pic = new Bitmap(@"D:\pipipupu\rr.jpg");
+        }
+    }
+    public class Drop
+    {
+        public int x, y;
+        //------------
+        /* public int h;
+         public Bitmap pic;*/
+        public Param p;
+        //------------
+        public Drop()
+        {
+            // h = 10;
+            x = 0; y = 0;
+        }
+        /* public Tree(int x, int y, int ah, Bitmap apic)
+         {
+             h = ah;
+             pic = apic;
+             this.x = x;
+             this.y = y;
+         }*/
+        public Drop(int x, int y, Param p)
+        {
+            this.p = p;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+    
---
+++ pp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+        Graphics g;
+        Drop[] t = new Drop[500];
+
+        Param[] p = new Param[2];
+        ParamWaterDrop p1 = new ParamWaterDrop();
+        ParamTwoWaterDrop p2 = new ParamTwoWaterDrop();
+        int count = 0;
+        Random r = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // r.Next(10, 800);
+
+            if (r.Next(0, 10) % 2 == 0)
+                t[count] = new Drop(r.Next(10, 1500), r.Next(10, 1500), p1);
+            else
+                t[count] = new Drop(r.Next(10, 1500), r.Next(10, 1500), p2);
+
+            g.DrawImage(t[count].p.pic, new Rectangle(t[count].x, t[count].y, t[count].p.h, 20));
+            if (count < 499)
+                count++;
+            else
+                timer1.Stop();
+        }
+    }
+}
